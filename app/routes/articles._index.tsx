c741078b9a6c2c752e10efebefbb644e5ecfc5b4
Jack Dewan <jdewan16@gmail.com>
2023-04-19 12:11:36 -0400

    add cta

--- app/routes/articles._index.tsx
+++ app/routes/articles._index.tsx
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { ALCard } from "~/components/ALCard";
+import { CTAFullScreen } from "~/components/CTAFullScreen";
 import { LearnIntro } from "~/components/LearnIntro";
 import { client } from "~/models/contentful.server";
 import { ArticleInterface } from "~/utils/interfaces";
@@ -17,13 +18,16 @@ export const loader = async () => {
 export default function Posts() {
   const { articles } = useLoaderData<typeof loader>();
   return (
-    <div className="container pb-10">
-      <LearnIntro />
-      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:gap-10 2xl:gap-20 lg:grid-cols-3 mx-auto">
-        {articles.map((article: ArticleInterface) => (
-          <ALCard key={article.slug} data={article} />
-        ))}
+    <div>
+      <div className="container pb-10">
+        <LearnIntro />
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:gap-10 2xl:gap-20 lg:grid-cols-3 mx-auto">
+          {articles.map((article: ArticleInterface) => (
+            <ALCard key={article.slug} data={article} />
+          ))}
+        </div>
       </div>
+      <CTAFullScreen dark />
     </div>
   );
 }
